1059d5f17616a8c9ae6fdb52f2faa0c12457614a
TBardini <tbardini@transperfect.com>
2024-05-22 21:15:11 -0400

    Update component imports to use shared directory

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -1,8 +1,8 @@
-import Card from "@/components/home/card";
+import Card from "@/components/shared/card";
 import { DEPLOY_URL } from "@/lib/constants";
 import { Github, Twitter } from "@/components/shared/icons";
 import WebVitals from "@/components/home/web-vitals";
-import ComponentGrid from "@/components/home/component-grid";
+import ComponentGrid from "@/components/shared/component-grid";
 import Image from "next/image";
 import { nFormatter } from "@/lib/utils";
 import Test from "@/app/test/page";
